fd8305b12e99f23d8432a15414d1961148a4d0f4
garywong4320-hub <gary.wong43.20@gmail.com>
2026-06-23 22:23:28 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,44 +1,82 @@
-from telegram import ChatPermissions
-from telegram.ext import Updater, MessageHandler, Filters
 import os
 import time
+from telegram import ChatPermissions, Update
+from telegram.ext import (
+    Application,
+    ContextTypes,
+    MessageHandler,
+    filters
+)
 
 TOKEN = os.getenv("BOT_TOKEN")
 
+
 FULL_PERMISSIONS = ChatPermissions(
     can_send_messages=True,
-    can_send_media_messages=True,
+    can_send_audios=True,
+    can_send_documents=True,
+    can_send_photos=True,
+    can_send_videos=True,
+    can_send_video_notes=True,
+    can_send_voice_notes=True,
     can_send_polls=True,
     can_send_other_messages=True,
-    can_add_web_page_previews=True,
-    can_invite_users=True
+    can_add_web_page_previews=True
 )
 
-def fix_permissions(chat_id, user_id, context):
-    for _ in range(3):
+
+async def fix_permissions(chat_id, user_id, context):
+
+    for i in range(3):
+
         try:
-            context.bot.restrict_chat_member(
+            await context.bot.restrict_chat_member(
                 chat_id=chat_id,
                 user_id=user_id,
                 permissions=FULL_PERMISSIONS
             )
+
             time.sleep(1)
+
         except Exception as e:
             print(e)
 
-def handle_new_members(update, context):
+
+
+async def handle_new_members(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    chat_id = update.effective_chat.id
+
     for user in update.message.new_chat_members:
+
+        print(f"Fixing permissions for {user.id}")
+
         time.sleep(2)
-        fix_permissions(update.effective_chat.id, user.id, context)
+
+        await fix_permissions(
+            chat_id,
+            user.id,
+            context
+        )
+
+
 
 def main():
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
 
-    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, handle_new_members))
+    app = Application.builder().token(TOKEN).build()
 
-    updater.start_polling()
-    updater.idle()
+    app.add_handler(
+        MessageHandler(
+            filters.StatusUpdate.NEW_CHAT_MEMBERS,
+            handle_new_members
+        )
+    )
+
+    print("Bot running")
+
+    app.run_polling()
+
+
 
 if __name__ == "__main__":
     main()
